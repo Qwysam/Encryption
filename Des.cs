da8b4c65b0804978bcaf2aa195475b1db5eddacd
Vsevolod Zhdanov <zhdanoov777@gmail.com>
2021-05-09 00:58:10 +0300

    Create Des.cs

---
+++ Des.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption
+{
+    class Des
+    {
+    	public byte[] GetKey()
+        {
+            DES tmp = DES.Create();
+            return tmp.Key;
+        }
+
+        public string EncryptData(string strData, byte[] Key)
+        {  
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray;
+
+            try
+            {
+                // DESCryptoServiceProvider is a cryptography class defind in c#.  
+                DESCryptoServiceProvider ObjDES = new DESCryptoServiceProvider();
+                inputByteArray = Encoding.UTF8.GetBytes(strData);
+                MemoryStream Objmst = new MemoryStream();
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateEncryptor(Key, IV), CryptoStreamMode.Write);
+                Objcs.Write(inputByteArray, 0, inputByteArray.Length);
+                Objcs.FlushFinalBlock();
+
+                return Convert.ToBase64String(Objmst.ToArray());//encrypted string  
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string DecryptData(string strData, byte[] Key)
+        {
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray = new byte[strData.Length];
+
+            try
+            {
+                DESCryptoServiceProvider ObjDES = new DESCryptoServiceProvider();
+                inputByteArray = Convert.FromBase64String(strData);
+
+                MemoryStream Objmst = new MemoryStream();
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateDecryptor(Key, IV), CryptoStreamMode.Write);
+                Objcs.Write(inputByteArray, 0, inputByteArray.Length);
+                Objcs.FlushFinalBlock();
+
+                Encoding encoding = Encoding.UTF8;
+                return encoding.GetString(Objmst.ToArray());
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
