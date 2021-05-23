50fe02cbe8ceef88d693385c45435143fc4e50e3
Vsevolod Zhdanov <61472372+Qwysam@users.noreply.github.com>
2021-05-23 23:07:52 +0300

    Add files via upload

---
+++ Blowfish.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Elskom.Generic.Libs;
+
+namespace Encryption
+{
+    class Blowfish
+    {
+        public BlowFish entity;
+
+        public Blowfish(byte[] key)
+        {
+            entity = new BlowFish(key);
+        }
+
+        public string Encrypt(string data)
+        {
+            byte[] tmp = Encoding.UTF8.GetBytes(data);
+            tmp = entity.EncryptCBC(tmp);
+            return Encoding.UTF8.GetString(tmp);
+        }
+
+        public string Decrypt(string data)
+        {
+            byte[] tmp = Encoding.UTF8.GetBytes(data);
+            tmp = entity.DecryptCBC(tmp);
+            return Encoding.UTF8.GetString(tmp);
+        }
+    }
+}
